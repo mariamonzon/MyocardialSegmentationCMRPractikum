ca0924ca3b5a64ba7ae72505af282edc8b2999ea
Mm24 <50300669+Mm24@users.noreply.github.com>
2020-07-22 00:23:31 +0200

    Update 3D UNet

--- model/unets.py
+++ model/unets.py
@@ -14,11 +14,13 @@ class conv_block(nn.Module):
         super(conv_block, self).__init__()
 
         self.conv = nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(out_ch),
+            nn.Conv3d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            # nn.Dropout3d(p=0.5),
+            nn.BatchNorm3d(out_ch),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(out_ch),
+            nn.Conv3d(out_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            # nn.Dropout2d(p=0.5),
+            nn.BatchNorm3d(out_ch),
             nn.ReLU(inplace=True))
 
     def forward(self, x):
@@ -35,8 +37,9 @@ class up_conv(nn.Module):
         super(up_conv, self).__init__()
         self.up = nn.Sequential(
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
-            nn.BatchNorm2d(out_ch),
+            nn.Conv3d(in_ch, out_ch, kernel_size=3, stride=1, padding=1, bias=True),
+            nn.Dropout3d(p=0.3),
+            nn.BatchNorm3d(out_ch),
             nn.ReLU(inplace=True)
         )
 
@@ -51,10 +54,10 @@ class U_Net(nn.Module):
     Paper : https://arxiv.org/abs/1505.04597
     """
 
-    def __init__(self, in_ch=3, out_ch=1):
+    def __init__(self, in_ch=3, out_ch=1, n_filters=32 ):
         super(U_Net, self).__init__()
 
-        n1 = 64
+        n1 = n_filters
         filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
 
         self.Maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
@@ -121,6 +124,84 @@ class U_Net(nn.Module):
         # d1 = self.active(out)
 
         return out
+
+
+
+class U_Net3D(nn.Module):
+    """
+    UNet - Basic Implementation
+    Paper : https://arxiv.org/abs/1505.04597
+    """
+
+    def __init__(self, in_ch=3, out_ch=1, n_filters=32 ):
+        super(U_Net3D, self).__init__()
+
+        n1 = n_filters
+        filters = [n1, n1 * 2, n1 * 4, n1 * 8, n1 * 16]
+
+        self.Maxpool = nn.MaxPool3d(kernel_size=2, stride=2)
+
+
+        self.Conv1 = conv_block(in_ch, filters[0])
+        self.Conv2 = conv_block(filters[0], filters[1])
+        self.Conv3 = conv_block(filters[1], filters[2])
+        self.Conv4 = conv_block(filters[2], filters[3])
+        self.Conv5 = conv_block(filters[3], filters[4])
+
+        self.Up5 = up_conv(filters[4], filters[3])
+        self.Up_conv5 = conv_block(filters[4], filters[3])
+
+        self.Up4 = up_conv(filters[3], filters[2])
+        self.Up_conv4 = conv_block(filters[3], filters[2])
+
+        self.Up3 = up_conv(filters[2], filters[1])
+        self.Up_conv3 = conv_block(filters[2], filters[1])
+
+        self.Up2 = up_conv(filters[1], filters[0])
+        self.Up_conv2 = conv_block(filters[1], filters[0])
+
+        self.Conv = nn.Conv2d(filters[0], out_ch, kernel_size=1, stride=1, padding=0)
+
+    # self.active = torch.nn.Sigmoid()
+
+    def forward(self, x):
+        e1 = self.Conv1(x)
+
+        e2 = self.Maxpool(e1)
+        e2 = self.Conv2(e2)
+
+        e3 = self.Maxpool(e2)
+        e3 = self.Conv3(e3)
+
+        e4 = self.Maxpool(e3)
+        e4 = self.Conv4(e4)
+
+        e5 = self.Maxpool(e4)
+        e5 = self.Conv5(e5)
+
+        d5 = self.Up5(e5)
+        d5 = torch.cat((e4, d5), dim=1)
+
+        d5 = self.Up_conv5(d5)
+
+        d4 = self.Up4(d5)
+        d4 = torch.cat((e3, d4), dim=1)
+        d4 = self.Up_conv4(d4)
+
+        d3 = self.Up3(d4)
+        d3 = torch.cat((e2, d3), dim=1)
+        d3 = self.Up_conv3(d3)
+
+        d2 = self.Up2(d3)
+        d2 = torch.cat((e1, d2), dim=1)
+        d2 = self.Up_conv2(d2)
+
+        out = self.Conv(d2)
+
+        # d1 = self.active(out)
+
+        return out
+
 
 
 class Recurrent_block(nn.Module):
@@ -722,3 +803,10 @@ class Nested_UNet(nn.Module):
 
         output = self.final(x0_4)
         return output
+
+if __name__ == '__main__':
+    model = U_Net3D(  in_ch=3, out_ch=1, n_filters=32 )
+    x = torch.rand(2, 3,32, 256, 256)
+    output = model(x)
+    print(output.shape)
+    print("finish")
